0b6c516f7dd3bdea02446ec6520414f72e342b4a
Yiping Lin <li1234355@gmail.com>
2023-08-03 16:30:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Kreativstorm Week 3 Assignment — JavaScript Part 1
+
+[View Live Demo](https://jpingr222.github.io/kreativstorm-week-3/)
+
+This is a JavaScript project of a game called Rock Paper Scissors! ✊✋✌️
+
+## 👥 Authors <a name="authors"></a>
+
+👤 **Yiping Lin**
+
+- GitHub: [@Yiping](https://github.com/jpingr222)
+- LinkedIn: [@Yiping](https://www.linkedin.com/in/lin-yiping)
+
+👤 **Mario Myrtaj**
+
+- GitHub: [@Myrtaj](https://github.com/mmyrtaj)
+- LinkedIn: [@Myrtaj](https://www.linkedin.com/in/mario-myrtaj-7b21b7171/)
+
+👤 **Amber "Mason" Stinchcomb**
+
+- GitHub: [@Stinchcomb](https://github.com/relentlessmason)
+- LinkedIn: [@Stinchcomb](https://www.linkedin.com/in/relentlessmason/)
+
+👤 **Abdulrahman Nganje**
+- GitHub: [@Nganje](https://github.com/asnganje)
+- Twitter: [@asnganje](https://twitter.com/asnganje)
+- LinkedIn: [@Nganje](https://www.linkedin.com/in/abdulrahman-nganje-a6436935/)
---
+++ index.js
@@ -0,0 +1,64 @@
+const validSelections = ['rock', 'paper', 'scissors'];
+
+const computerPlay = () => {
+  return validSelections[Math.floor(Math.random() * validSelections.length)];
+}
+
+const playerIsWinner = (playerSelection, computerSelection) => {
+  return (
+    validSelections.indexOf(playerSelection) - validSelections.indexOf(computerSelection) === 1 ||
+    (playerSelection === 'rock' && computerSelection === 'scissors')
+  );
+}
+
+const playRound = (playerSelection, computerSelection) => {
+  const lowerCasePlayerSelection = playerSelection.toLowerCase();
+
+  if (!validSelections.includes(lowerCasePlayerSelection)) {
+    return 'Invalid selection! Please select again';
+  }
+
+  if (playerSelection === computerSelection) {
+    return `It's a tie! You both picked ${computerSelection}`;
+  } else if (playerIsWinner(playerSelection, computerSelection)) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+const game = () => {
+  console.log('Welcome to Rock Paper Scissors!');
+
+  const gameRoundStatus = [];
+  while (gameRoundStatus.length < 5) {
+    const playerSelection = prompt('Please enter your selection (rock, paper, or scissors)');
+    const computerSelection = computerPlay();
+
+    const gameResult = playRound(playerSelection, computerSelection);
+
+    console.log('==========');
+    console.log(`Round ${gameRoundStatus.length + 1}:`);
+    console.log(gameResult);
+
+    if (gameResult.includes('Win')) {
+      gameRoundStatus.push(1);
+    } else if (gameResult.includes('Lose')) {
+      gameRoundStatus.push(-1);
+    } else if (gameResult.includes('tie')) {
+      gameRoundStatus.push(0);
+    }
+  }
+
+  const winRoundCount = gameRoundStatus.filter(result => result === 1).length;
+  const loseRoundCount = gameRoundStatus.filter(result => result === -1).length;
+  const tieRoundCount = gameRoundStatus.filter(result => result === 0).length;
+
+  console.log('==========');
+  console.log('Game Over');
+  console.log(`You won ${winRoundCount} round${winRoundCount > 1 ? 's' : ''}`);
+  console.log(`You lost ${loseRoundCount} round${loseRoundCount > 1 ? 's' : ''}`);
+  console.log(`You tied ${tieRoundCount} round${tieRoundCount > 1 ? 's' : ''}`);
+}
+
+game();
