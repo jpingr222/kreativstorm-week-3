4bee437072767d8447c3c52c2672be3694f296ee
mmyrtaj <mario.myrtaj@gmail.com>
2023-08-04 20:03:12 +0200

    Fixed input when entering no characters

--- index.js
+++ index.js
@@ -1,75 +1,63 @@
 const validSelections = ['rock', 'paper', 'scissors'];
 
 const computerPlay = () => {
-  return validSelections[Math.floor(Math.random() * validSelections.length)];
+    return validSelections[Math.floor(Math.random() * validSelections.length)];
 }
 
 const playerIsWinner = (playerSelection, computerSelection) => {
-  return (
-    validSelections.indexOf(playerSelection) - validSelections.indexOf(computerSelection) === 1 ||
-    (playerSelection === 'rock' && computerSelection === 'scissors')
-  );
+    return (
+        validSelections.indexOf(playerSelection) - validSelections.indexOf(computerSelection) === 1 ||
+        (playerSelection === 'rock' && computerSelection === 'scissors')
+    );
 }
 
 const playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === '') {
-    return 'Invalid selection! \u26A0 Please select again';
-  }
-
-  const lowerCasePlayerSelection = playerSelection.toLowerCase();
-  
-  const userAndCompSelection = {
-    playerSelection: lowerCasePlayerSelection[0].toUpperCase()
-    .concat(lowerCasePlayerSelection.slice(1)),
-    computerSelection: computerSelection[0].toUpperCase()
-    .concat(computerSelection.slice(1))
-  };
-
-  if (!validSelections.includes(lowerCasePlayerSelection)) {
-    return 'Invalid selection! \u26A0 Please select again';
-  }
-
-  if (lowerCasePlayerSelection === computerSelection) {
-    return `It's a tie! \uD83E\uDD14 You both picked ${userAndCompSelection['computerSelection']}`;
-  } else if (playerIsWinner(lowerCasePlayerSelection, computerSelection)) {
-    return `You Win! \uD83C\uDF89 ${userAndCompSelection['playerSelection']} beats ${userAndCompSelection['computerSelection']}`;
-  } else {
-    return `You Lose! \uD83D\uDE1E ${userAndCompSelection['computerSelection']} beats ${userAndCompSelection['playerSelection']}`;
-  }
+    const lowerCasePlayerSelection = playerSelection.toLowerCase();
+    if (playerSelection === null || !validSelections.includes(lowerCasePlayerSelection)) {
+        return 'Invalid selection! \u26A0 Please select again';
+    }else{
+        const userAndCompSelection = {
+            playerSelection: lowerCasePlayerSelection[0].toUpperCase()
+            .concat(lowerCasePlayerSelection.slice(1)),
+            computerSelection: computerSelection[0].toUpperCase()
+            .concat(computerSelection.slice(1))
+            };
+        if (lowerCasePlayerSelection === computerSelection) {
+            return `It's a tie! \uD83E\uDD14 You both picked ${userAndCompSelection['computerSelection']}`;
+        } else if (playerIsWinner(lowerCasePlayerSelection, computerSelection)) {
+            return `You Win! \uD83C\uDF89 ${userAndCompSelection['playerSelection']} beats ${userAndCompSelection['computerSelection']}`;
+        } else {
+            return `You Lose! \uD83D\uDE1E ${userAndCompSelection['computerSelection']} beats ${userAndCompSelection['playerSelection']}`;
+        }
+    }
 }
 
 const game = () => {
-  console.log('Welcome to Rock Paper Scissors!');
-
-  const gameRoundStatus = [];
-  while (gameRoundStatus.length < 5) {
-    const playerSelection = prompt('Open your console.log to play- Please enter your selection (rock, paper, or scissors)');
-    const computerSelection = computerPlay();
-
-    const gameResult = playRound(playerSelection, computerSelection);
-
-    console.log('==========');
-    console.log(`Round ${gameRoundStatus.length + 1}:`);
-    console.log(gameResult);
-
-    if (gameResult.includes('Win')) {
-      gameRoundStatus.push(1);
-    } else if (gameResult.includes('Lose')) {
-      gameRoundStatus.push(-1);
-    } else if (gameResult.includes('tie')) {
-      gameRoundStatus.push(0);
+    console.log('Welcome to Rock Paper Scissors!');
+    const gameRoundStatus = [];
+    while (gameRoundStatus.length < 5) {
+        const playerSelection = prompt('Open your console.log to play- Please enter your selection (rock, paper, or scissors)');
+        const computerSelection = computerPlay();
+        const gameResult = playRound(playerSelection, computerSelection);
+        console.log('==========');
+        console.log(`Round ${gameRoundStatus.length + 1}:`);
+        console.log(gameResult);
+        if (gameResult.includes('Win')) {
+            gameRoundStatus.push(1);
+        } else if (gameResult.includes('Lose')) {
+            gameRoundStatus.push(-1);
+        } else if (gameResult.includes('tie')) {
+            gameRoundStatus.push(0);
+        }
     }
-  }
-
-  const winRoundCount = gameRoundStatus.filter(result => result === 1).length;
-  const loseRoundCount = gameRoundStatus.filter(result => result === -1).length;
-  const tieRoundCount = gameRoundStatus.filter(result => result === 0).length;
-
-  console.log('==========');
-  console.log('Game Over');
-  console.log(`You won ${winRoundCount} round${winRoundCount > 1 ? 's' : ''}`);
-  console.log(`You lost ${loseRoundCount} round${loseRoundCount > 1 ? 's' : ''}`);
-  console.log(`You tied ${tieRoundCount} round${tieRoundCount > 1 ? 's' : ''}`);
+    const winRoundCount = gameRoundStatus.filter(result => result === 1).length;
+    const loseRoundCount = gameRoundStatus.filter(result => result === -1).length;
+    const tieRoundCount = gameRoundStatus.filter(result => result === 0).length;
+    console.log('==========');
+    console.log('Game Over');
+    console.log(`You won ${winRoundCount} round${winRoundCount > 1 ? 's' : ''}`);
+    console.log(`You lost ${loseRoundCount} round${loseRoundCount > 1 ? 's' : ''}`);
+    console.log(`You tied ${tieRoundCount} round${tieRoundCount > 1 ? 's' : ''}`);
 }
 
 game();
