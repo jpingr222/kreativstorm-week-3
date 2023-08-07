3db7b3f168b24792727c40db6bc4e41bd176e25a
mmyrtaj <38790220+mmyrtaj@users.noreply.github.com>
2023-08-07 18:39:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const playRound = (playerSelection, computerSelection) => {
     let lowerCasePlayerSelection = (
         playerSelection === null
         ? null
-        : playerSelection.toLowerCase().strip();
+        : playerSelection.toLowerCase()..replace(/ /g, "");
     );
 
     if (playerSelection === null || !validSelections.includes(lowerCasePlayerSelection)) {
