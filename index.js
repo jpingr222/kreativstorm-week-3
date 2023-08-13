e5badfd2db13cbc1a5f735ffd44bd240292a8776
asnganje <abd.nganje@gmail.com>
2023-08-13 09:18:52 +0300

    remove lowercase conversion from playRound

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ const endGameDialogue =(winRoundCount) => {
     }
 
 const playRound = (playerSelection, computerSelection) => {
-    let lowerCasePlayerSelection = playerSelection !== null?playerSelection.toLowerCase().replace(/\s/g, ''):playerSelection;
 
     if (playerSelection === null || !validSelections.includes(lowerCasePlayerSelection)) {
         return 'Invalid selection! \u26A0 Please select again';
