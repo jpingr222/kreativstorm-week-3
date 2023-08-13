bc677aee0943f6b1d3ca165f380415075b03a9da
Mason <104283721+relentlessmason@users.noreply.github.com>
2023-08-12 20:05:03 -0400

    i'm seriously about to give up

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const playRound = (playerSelection, computerSelection) => {
 
     if (playerSelection === null) {
         return;
-    } else {!validSelections.includes(lowerCasePlayerSelection) {
+    } else { (!validSelections.includes(lowerCasePlayerSelection)) {
         return 'Invalid selection! \u26A0 Please select again';
     } else {
         let userAndCompSelection = {
