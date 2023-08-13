345ef63be855c81ded4a321996f6f9ec27e17425
Mason <104283721+relentlessmason@users.noreply.github.com>
2023-08-12 20:55:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -44,33 +44,28 @@ const endGameDialogue =(winRoundCount) => {
     }
 
 const playRound = (playerSelection, computerSelection) => {
-    try{ 
-        let lowerCasePlayerSelection = (
-        playerSelection === null
-        ? null
-        : playerSelection.toLowerCase().trim());
+    let lowerCasePlayerSelection = (
+    playerSelection === null
+    ? null
+    : playerSelection.toLowerCase().trim());
 
-        if (playerSelection === null || !validSelections.includes(lowerCasePlayerSelection)) {
-            return 'Invalid selection! \u26A0 Please select again';
+    if (playerSelection === null || !validSelections.includes(lowerCasePlayerSelection)) {
+        return 'Invalid selection! \u26A0 Please select again';
+    } else {
+        let userAndCompSelection = {
+            playerSelection: lowerCasePlayerSelection[0]
+                .toUpperCase()
+                .concat(lowerCasePlayerSelection.slice(1)),
+            computerSelection: computerSelection[0]
+                .toUpperCase()
+                .concat(computerSelection.slice(1))
+        };
+        if (lowerCasePlayerSelection === computerSelection) {
+            return `It's a tie! \uD83E\uDD14 You both picked ${userAndCompSelection['computerSelection']}`;
+        } else if (playerIsWinner(lowerCasePlayerSelection, computerSelection)) {
+             return `You Win! ${addFunnyMessage(true)} \uD83C\uDF89 ${userAndCompSelection['playerSelection']} beats ${userAndCompSelection['computerSelection']}`;
         } else {
-            let userAndCompSelection = {
-                playerSelection: lowerCasePlayerSelection[0]
-                    .toUpperCase()
-                    .concat(lowerCasePlayerSelection.slice(1)),
-                computerSelection: computerSelection[0]
-                    .toUpperCase()
-                    .concat(computerSelection.slice(1))
-            };
-            if (lowerCasePlayerSelection === computerSelection) {
-                return `It's a tie! \uD83E\uDD14 You both picked ${userAndCompSelection['computerSelection']}`;
-            } else if (playerIsWinner(lowerCasePlayerSelection, computerSelection)) {
-                return `You Win! ${addFunnyMessage(true)} \uD83C\uDF89 ${userAndCompSelection['playerSelection']} beats ${userAndCompSelection['computerSelection']}`;
-            } else {
-                return `You Lose! ${addFunnyMessage(false)} \uD83D\uDE1E ${userAndCompSelection['computerSelection']} beats ${userAndCompSelection['playerSelection']}`;
-            }
-        }
-        catch (null) {
-            return 'bye';
+             return `You Lose! ${addFunnyMessage(false)} \uD83D\uDE1E ${userAndCompSelection['computerSelection']} beats ${userAndCompSelection['playerSelection']}`;
         }
     }
 }
